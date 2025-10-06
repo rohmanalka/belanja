6d3598580655b5e704501c5d39e2eeddd5b79293
rohmanalka <rohmanalka06@gmail.com>
2025-10-06 22:15:57 +0700

    feat: define model

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import '../models/item.dart';
 
 class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000)
+  ];
+
   @override
   Widget build(BuildContext context) {
-    
+    // TODO: implement build
     throw UnimplementedError();
   }
 }
